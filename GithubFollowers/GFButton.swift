7364118e9ef06ab840899b04d61e8c1327cf7efc
Carrington <Manyuchi>
2024-01-05 14:53:02 +0200

    Added Custom Button

---
+++ GithubFollowers/GFButton.swift
@@ -0,0 +1,36 @@
+//
+//  GFButton.swift
+//  GithubFollowers
+//
+//  Created by DA MAC M1 157 on 2024/01/05.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+        // subclassing an object. wea re building around what we have been provided by default
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
--- GithubFollowers/MainTabBarViewController.swift
+++ GithubFollowers/MainTabBarViewController.swift
@@ -11,19 +11,30 @@ class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupTabs()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupTabs() {
+        
+        let favorites = UINavigationController(rootViewController: FavoritesListViewController())
+        let search = UINavigationController(rootViewController: SearchViewController())
+        
+        favorites.title = "Favorites"
+        search.title = "Search"
+        
+        favorites.tabBarItem.title = "Favorites"
+        search.tabBarItem.title = "Search"
+        
+        favorites.tabBarItem.image = UIImage(systemName: "star")
+        search.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        
+        favorites.tabBarItem.selectedImage = UIImage(systemName: "star.fill")
+        search.tabBarItem.selectedImage = UIImage(systemName: "magnifyingglass.fill")
+                
+        UITabBar.appearance().tintColor = .systemGreen
+        UITabBar.appearance().backgroundColor = .systemBackground
+        
+        setViewControllers([favorites, search], animated: true)
     }
-    */
 
 }
