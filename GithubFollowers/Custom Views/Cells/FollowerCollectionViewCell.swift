0be86d9846e188ada7a88cd8695541d81244b340
Carrington <Manyuchi>
2024-01-09 14:10:55 +0200

    network call part 2

---
+++ GithubFollowers/Custom Views/Cells/FollowerCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FollowerCollectionViewCell.swift
+//  GithubFollowers
+//
+//  Created by DA MAC M1 157 on 2024/01/08.
+//
+
+import UIKit
+
+class FollowerCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ GithubFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,20 @@
+//
+//  GFAvatarImageView.swift
+//  GithubFollowers
+//
+//  Created by DA MAC M1 157 on 2024/01/08.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
