feb3d3c620b1a58c78a423297b371d3c6e902e7f
Carrington <Manyuchi>
2024-01-13 07:28:10 +0200

    Formatting DateLabel

---
+++ GithubFollowers/Extensions/Date+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  Date+Ext.swift
+//  GithubFollowers
+//
+//  Created by DA MAC M1 157 on 2024/01/13.
+//
+
+import Foundation
---
+++ GithubFollowers/Extensions/String+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  String+Ext.swift
+//  GithubFollowers
+//
+//  Created by DA MAC M1 157 on 2024/01/13.
+//
+
+import Foundation
