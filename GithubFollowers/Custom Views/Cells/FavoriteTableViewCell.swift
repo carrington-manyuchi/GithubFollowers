2d9d979553019667b1be33490cef0d7c1e064686
Carrington <Manyuchi>
2024-01-14 09:18:32 +0200

    Persistence Manager

---
+++ GithubFollowers/Custom Views/Cells/FavoriteTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FavoriteTableViewCell.swift
+//  GithubFollowers
+//
+//  Created by DA MAC M1 157 on 2024/01/14.
+//
+
+import UIKit
+
+class FavoriteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ GithubFollowers/Managers/PersistenceManager.swift
@@ -0,0 +1,8 @@
+//
+//  PersistenceManager.swift
+//  GithubFollowers
+//
+//  Created by DA MAC M1 157 on 2024/01/14.
+//
+
+import Foundation
